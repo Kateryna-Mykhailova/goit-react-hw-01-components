332684bfc68122e8c6776eff96dc67bff73e65a9
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-10-03 14:37:32 +0200

    Добавляет стартовые файлы

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+    
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+const element = React.createElement('div', {a:5, children: 'Привет, мир'});
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'))

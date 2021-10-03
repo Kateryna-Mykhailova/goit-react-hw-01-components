7d34331347030b23292ac692cbf234e66448fb9f
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-10-03 15:57:43 +0200

    Добавляет стартовые файлы

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,8 @@
+export function Gallery() {
+  return (
+    <section>
+      <h2>Gallery</h2>
+      <p> dfghjo;loiuyt fgh</p>
+    </section>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-    
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
-const element = React.createElement('div', {a:5, children: 'Привет, мир'});
+const element = React.createElement('div', { a: 5, children: 'Привет, мир' });
 console.log(element);
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById('root'));
+
+let value = 'click';
+const newBtn = <button type-button>{value}</button>;
+console.log(newBtn);
